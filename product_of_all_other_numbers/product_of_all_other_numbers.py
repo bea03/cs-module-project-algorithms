d46defaf954ce338d2a3bbb27f5d93b0c6f6061c
Nicole Rodriguez <tulip@Nicoles-Air.hsd1.ut.comcast.net>
2020-06-18 15:50:32 -0600

    plan

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -1,11 +1,34 @@
 '''
-Input: a List of integers
-Returns: a List of integers
+Write a function that receives an array of integers and returns
+an array consisting of the product of all numbers in the array
+except the number at that index.
+For example, given
+
+[1, 7, 3, 4]
+
+your function should return
+
+[84, 12, 28, 21]
+
+by calculating
+
+[7*3*4, 1*3*4, 1*7*4, 1*7*3]
+
+In the above example, the final value at index 0
+is the product of every number in the input array
+except the number at index 0.
+
+Can you do this in O(n) time with O(1) space without using division?
 '''
+import math
+
 def product_of_all_other_numbers(arr):
     # Your code here
+    #return a new array with products
+    pro_arr = []
 
-    pass
+    #loop through once and place in pro_arr products
+    
 
 
 if __name__ == '__main__':
